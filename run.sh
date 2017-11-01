ee78bbe0a4bdfc82e5c9034c88f1383cae94fd40
vattay <3martin@gmail.com>
2017-11-01 06:59:57 -0400

    Make volume specific to testnet.

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 source .btcgpu-docker-env
-docker run -v btcgpu-vol:/root/.bitcoingold -p $BG_PORT:$BG_PORT -d --rm $BG_IMAGE bgoldd --printtoconsole --testnet $@
+docker run -v btcgpu-testnet-vol:/root/.bitcoingold -p $BG_PORT:$BG_PORT -d --rm $BG_IMAGE bgoldd --printtoconsole --testnet $@
     
