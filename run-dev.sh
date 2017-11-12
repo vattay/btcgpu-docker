f582b9520320ac2c2a70e5c55307fbfaa1df740e
vattay <3martin@gmail.com>
2017-11-11 19:50:56 -0500

    Missing files from last commit.

--- run-dev.sh
+++ run-dev.sh
@@ -2,5 +2,5 @@
 
 source .btcgpu-docker-env
 
-docker run -it -v btcgpu-vol:/root/.bitcoingold --rm ${BG_IMAGE}-dev 
+docker run -it -v $BG_VOL:/root/.bitcoingold --rm ${BG_IMAGE}-dev
     
--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 source .btcgpu-docker-env
-docker run -v btcgpu-testnet-vol:/root/.bitcoingold -p $BG_PORT:$BG_PORT -p $BG_RPC_PORT:$BG_RPC_PORT -d --rm --name $BG_CONTAINER $BG_IMAGE bgoldd --printtoconsole --addnode=eunode.pool.gold --addnode=asianode.pool.gold --testnet --txindex $@
-    
+docker run -v $BG_VOL:/root/.bitcoingold -p $BG_PORT:$BG_PORT -p $BG_RPC_PORT:$BG_RPC_PORT -d --rm --name $BG_CONTAINER $BG_IMAGE bgoldd --printtoconsole --addnode=eunode.pool.gold --addnode=asianode.pool.gold --testnet --txindex $@
